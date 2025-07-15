fe13f4ea23cf86072a168b7589c53d1d4b234343
PTregembo <tregembop@gmail.com>
2025-07-15 12:17:34 -0500

    Task 1 completed

--- client.c
+++ client.c
@@ -11,9 +11,16 @@
 queue_t *chunk_queue;
 int digit_counts[10] = {0};
 #define MAX_SIZE 1024
+
+//for synch
+pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t not_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t not_full = PTHREAD_COND_INITIALIZER;
+
+//struct def
 typedef struct chunk {
     char data[MAX_SIZE]; // Buffer to hold chunk data
-    int length;          // Length of the data in the chunk
+    int length; // Length of the data in the chunk
 } chunk_t;
 
 typedef struct queue {
@@ -21,7 +28,7 @@ typedef struct queue {
     int rear;
     int size;
     int capacity;
-    chunk_t *chunks; // Array of chunks
+    chunk_t *chunks; // ARRAY of chunks
 } queue_t;
 
 
@@ -50,6 +57,8 @@ queue_t* create_queue(int qsize) {
     return q;
 }
 
+
+
 void destroy_queue(queue_t *q) {
     // TODO: Free queue memory
     if (q != NULL) {
@@ -61,14 +70,62 @@ void destroy_queue(queue_t *q) {
     }
 }
 
+
+
 int enqueue(queue_t *q, chunk_t *chunk) {
     // TODO: Add chunk to queue
-    return 0;
+    //need to prevent race conditions when multiple threads try to enqueue/dequeue concorrently 
+    pthread_mutex_lock(&queue_mutex); //lock mutex for exclusive acces to the queue data structure 
+
+    //if the queue is full, wait on the not_full condition variable
+    //this means the producer thread will SLEEP here until a worker thread signals that a slot has been freed (by dequeuing a chunk) 
+    while (q->size == q->capacity) {
+        //this atomically releases the mutex and suspends the thread
+        //when the thread wakes up, it re-aquires the mutex before returning 
+        pthread_cond_wait(&not_full, &queue_mutex); //this waits if the queue is FULL 
+    //return 0;
+    }
+
+    
+    q->chunks[q->rear] = *chunk; //copy chunk data into the position indicated by the rear index in the circular buffer
+    q->rear= [q->rear+1] % q->capacity; //update rear index to the NEXT slot in the circular queue, % wraps around when reaching the end of the array 
+    q->size++; //must increment size to reflect the ADDED chunk 
+
+    pthread_cond_signal(&not_empty); //signal the not_empty condition variable to wake up any threads WAITING to dequeu, queue not has at least one item
+    pthread_mutex_unlock(&queue_mutex); // unlock the mutex to allow other threads to access the queue 
+    return 0; //success 
 }
+
+
+
+
 
 chunk_t* dequeue(queue_t *q) {
     // TODO: Remove and return chunk from queue
-    return NULL;
+    // Lock the mutex for exclusive access to the queue 
+    pthread_mutex_lock(&queue_mutex);
+
+    //if queue is empty, wait on the not_empty condition variable 
+    //this means the consumer thread will sleep here until a producer signals that a chunk has been ENQUEUED 
+    while (q->size==0) {
+        pthread_cond_wait(&not_empty, &queue_mutex); //waits if queue is EMPTY, releases mutex and suspends thread until signaled 
+    }
+    //must allocate memory for a chunk pointer that will hold the dequeued data 
+    chunk_t *chunk = malloc(sizeof(chunk_t));
+    if(chunk ==NULL){
+        //if memory allocation fails- print error message AND unlock mutex, returns null 
+        fprintf(stderr, "Failed to allocate memory for dequeued chunk\n");
+        pthread_mutex_unlock(&queue_mutex);
+        return NULL;
+    }
+    //copy that chunk data from the position indicated by front in the queue
+    *chunk = q->chunks[q->front];
+    q->front = (q->front +1) % q->capacity; //update front index to NEXT slot, 
+    q->size--; //must decrement the queue size to reflect REMOVAL 
+
+    pthread_cond_signal(&not_full); //signal the not_full condition vairable to wake up any threads waiting to enqueue
+    pthread_mutex_unlock(&queue_mutex); // unlock the mutex so other threads can access the queue 
+    return chunk; //return the POINTER to the dequeued chunk 
 }
 
 // Worker thread function
